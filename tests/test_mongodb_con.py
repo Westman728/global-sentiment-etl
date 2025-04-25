41c28135adc7e655bc34f48e4cd5362cae9a5a9e
Westman728 <r.westman93@gmail.com>
2025-04-25 12:19:56 +0200

    fix: update MongoDB settings to load from credentials.yaml; remove deprecated test file

--- tests/test_mongodb_con.py
+++ tests/test_mongodb_con.py
@@ -5,17 +5,14 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logger.info(f"Testing MongoDB connection using hardcoded values.")
-client = MongoClient("mongodb://localhost:27018")
-logger.info(f"MongoDB database names: {client.list_database_names()}")
-logger.info(f"MongoDB collection count: {client["global_sentiment"]["raw_reddit_posts"].count_documents({})}")
-logger.info(f"First document in collection: {client["global_sentiment"]["raw_reddit_posts"].find_one({})}")
-
 logger.info(f"Testing MongoDB connection using YAML config.")
 with open("config/settings.yaml", "r") as file:
-    config = yaml.safe_load(file)
-    client = MongoClient(config["mongodb"]["connection_string"])
-    logger.info(f"MongoDB connection string: {config['mongodb']['connection_string']}")
-    logger.info(f"MongoDB database names: {client.list_database_names()}")
-    logger.info(f"MongoDB collection count: {client['global_sentiment']['raw_reddit_posts'].count_documents({})}")
+    settings = yaml.safe_load(file)
+with open("config/credentials.yaml", "r") as cred_file:
+    credentials = yaml.safe_load(cred_file)
+settings["mongodb"] = credentials["mongodb"]
+client = MongoClient(settings["mongodb"]["connection_string"])
+logger.info(f"MongoDB connection string: {settings['mongodb']['connection_string']}")
+logger.info(f"MongoDB database names: {client.list_database_names()}")
+logger.info(f"MongoDB collection count: {client['global_sentiment']['raw_reddit_posts'].count_documents({})}")
 
